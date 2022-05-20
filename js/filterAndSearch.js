c36165c218a9f4feb0b21c1dac89827ea28fc7a0
Maiken Lindstad <54981180+maikenlindstad@users.noreply.github.com>
2022-05-20 23:49:13 +0200

    Backup

--- js/filterAndSearch.js
+++ js/filterAndSearch.js
@@ -4,13 +4,13 @@ const productsContainer = document.querySelector(".products-container");
 const search = document.querySelector(".search");
 
 // let productsToRender = products;
+// let productsToRender = [];
 
 async function renderProducts() {
   try {
     const response = await fetch(url);
     const results = await response.json();
     productsContainer.innerHTML = "";
-
 
     // results.data instead of teamsToRender
     results.data.forEach(function (product) {
@@ -21,43 +21,47 @@ async function renderProducts() {
         </div>`;
     });
 
+
+    function searchProducts(products) {
+
+      search.onkeyup = () => {
+
+        const searchValue = event.target.value.trim().toLowerCase();
+
+        const filteredProducts = products.data.filter(function (product) {
+          if (product.attributes.title.toLowerCase().startsWith(searchValue)) {
+            return true;
+          }
+        });
+
+        console.log(filteredProducts);
+
+        filteredProducts.forEach(product => {
+          console.log(product.attributes.title);
+
+          productsContainer.innerHTML = "";
+
+          renderProducts();
+
+          productsContainer.innerHTML += `
+            <div class="product">
+              <img src="${product.attributes.image.data[0].attributes.url}" style="width: 100px; ">
+              <h2>${product.attributes.title}</h2>
+            </div>`;
+        });
+
+        // renderProducts(filteredProducts);
+
+      };
+
+    }
+    renderProducts(results);
+
     searchProducts(results);
+
 
   } catch (error) {
     console.log(error);
   }
 }
 renderProducts();
-
-function searchProducts(products) {
-
-  search.onkeyup = () => {
-    // console.log(event);
-
-    const searchValue = event.target.value.trim().toLowerCase();
-
-    const filteredProducts = products.data.filter(function (product) {
-      if (product.attributes.title.toLowerCase().startsWith(searchValue)) {
-        return true;
-      }
-    });
-
-    console.log(filteredProducts);
-
-    renderProducts();
-
-    filteredProducts.forEach(product => {
-      console.log(product.attributes.title)
-      // renderProducts();
-
-      productsContainer.innerHTML = "";
-
-      productsContainer.innerHTML += `
-        <div class="product">
-          <img src="${product.attributes.image.data[0].attributes.url}" style="width: 100px; ">
-          <h2>${product.attributes.title}</h2>
-        </div>`;
-    });
-    renderProducts(filteredProducts);
-  };
-}
